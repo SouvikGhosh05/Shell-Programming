56e4cbc1e7941b3027a577f76adf2e22af121950
souvikghosh05 <souvikghosh872@gmail.com>
2022-03-11 00:39:18 +0530

    Makefile created

---
+++ Makefile
@@ -0,0 +1,6 @@
+build:
+	chmod +x ./shc_build.sh
+	./shc_build.sh
+
+clean:
+	rm -rf ./bin
---
+++ shc_build.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+
+if which shc > /dev/null; then
+    echo "shc already installed"
+else
+    echo "shc not installed, installing now"
+    sudo apt install software-properties-common -y
+    sudo add-apt-repository ppa:neurobin/ppa -y
+    sudo apt-get update
+    sudo apt-get install shc -y
+fi
+
+DIR="src/*.sh"
+
+build_binary() {
+    for filename in $DIR; do
+        
+        name=${filename##*/} 
+        shc -f "src/$name" -o "bin/${name%%.*}"
+    done
+}
+
+if [ ! -d "bin" ]; then     # if bin directory does not exist
+    mkdir bin
+    build_binary
+else 
+    build_binary
+fi
