b94c915fe38445ec96ec9c1f5acf096710f49be4
souvikghosh05 <souvikghosh872@gmail.com>
2022-03-15 00:11:41 +0530

    Comments are fixed👌

--- shc_build.sh
+++ shc_build.sh
@@ -14,7 +14,7 @@ build_binary() {
     for filename in $DIR; do
         
         name=${filename##*/}                        # Remove pattern from left, e.g. "src/foo.sh" -> "foo.sh" where, pattern is "/
-        shc -f "src/$name" -o "bin/${name%%.*}"     # Remove pattern from right e.g. "foo.sh" -> "foo" where, pattern is "."
+        shc -f "src/$name" -o "bin/${name%%.*}"     # Remove pattern from right, e.g. "foo.sh" -> "foo" where, pattern is "."
     done
 }
 
